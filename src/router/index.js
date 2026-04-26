1ef516b683b46ac9c7da706a870bf277e13ff596
Nurseto <raihannt12345@gmail.com>
2026-04-26 13:53:26 +0700

    fix: memperbaiki typo huruf kapital pada import DashBoardView

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-import DashboardView from '../views/DashboardView.vue'
+import DashBoardView from '../views/DashBoardView.vue'
 
 const routes = [
   { path: '/', component: LoginView },
-  { path: '/dashboard', component: DashboardView }
+  { path: '/dashboard', component: DashBoardView }
 ]
 
 const router = createRouter({
